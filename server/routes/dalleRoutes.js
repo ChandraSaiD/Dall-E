b34da57545a07d986dd5149aa0e01c52eec496dd
Chandra Sai D <chandrasaic39333@gmail.com>
2024-05-01 19:07:06 +0530

    initial commit

--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -1,16 +1,16 @@
 import express from 'express';
 import * as dotenv from 'dotenv';
-import OpenAI from 'openai';
+import {OpenAI} from 'openai';
 
-dotenv.config();
+// dotenv.config();
 
 const router = express.Router();
 
-const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY,
-});
+const openai = new OpenAI({apiKey: process.env.OPENAI_API_KE});
+
 router.route('/').get((req, res) => {
-  res.status(200).json({ message: 'Hello from DALL-E!' });
+  // res.status(200).json({ message: 'Hello from DALL-E!' });
+  res.send('hello from DALL-E');
 });
 
 router.route('/').post(async (req, res) => {
@@ -24,11 +24,13 @@ router.route('/').post(async (req, res) => {
       response_format: 'b64_json',
     });
 
-    const image = aiResponse.data.data[0].b64_json;
+    // const image = aiResponse.data.data[0].b64_json;
+    const image = aiResponse.data[0].b64_json;
+
     res.status(200).json({ photo: image });
   } catch (error) {
     console.error(error);
-    res.status(500).send(error?.response?.data?.error?.message || 'Something went wrong');
+    res.status(500).send(error?.response.data.error.message)
 
   }
 });
